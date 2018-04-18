622a32ed7eadc1f26edb80e01b6512d703f436f7
Janis Giovani Tan <janist@id.ibm.com>
2018-04-18 08:09:10 +0700

    remove text to speecha nd speech to text

--- controller/watson-controller.js
+++ controller/watson-controller.js
@@ -1,13 +1,6 @@
 const Promise = require('bluebird');
 const cfenv = require('./../config/cfenv-init.js');
 const ConversationV1 = require('watson-developer-cloud/conversation/v1');
-const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
-
-const text_to_speech = new TextToSpeechV1({
-    url: cfenv.services.text_to_speech[0].credentials.url,
-    username: cfenv.services.text_to_speech[0].credentials.username,
-    password: cfenv.services.text_to_speech[0].credentials.password,
-});
 
 const conversation = new ConversationV1({
     url: cfenv.services.conversation[0].credentials.url,
@@ -32,14 +25,4 @@ exports.sendConversation = (req, res, next) => {
         .catch((err) => {
             res.send(err);
         });
-};
-
-exports.tts = (req, res, next) => {
-    var params = {
-        text: req.body.text,
-        voice: 'en-US_AllisonVoice', // Optional voice
-        accept: 'audio/wav'
-    };
-    // Pipe the synthesized text to a file
-    text_to_speech.synthesize(params).pipe(res);
 };
